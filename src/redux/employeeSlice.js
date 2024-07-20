0ab5edaef97bde9dae87f8a822f7e2066903423c
stelda <cristel.darre@outlook.com>
2024-07-20 10:53:56 +0200

    Add redux, redux toolkit and react-router-dom to project

---
+++ src/redux/employeeSlice.js
@@ -0,0 +1,14 @@
+﻿import { createSlice } from '@reduxjs/toolkit';
+
+const employeeSlice = createSlice({
+    name: 'employees',
+    initialState: [],
+    reducers: {
+        addEmployee: (state, action) => {
+            state.push(action.payload);
+        }
+    }
+});
+
+export const { addEmployee } = employeeSlice.actions;
+export default employeeSlice.reducer;
---
+++ src/redux/store.js
@@ -0,0 +1,10 @@
+﻿import { configureStore } from '@reduxjs/toolkit';
+import employeeReducer from './employeeSlice';
+
+const store = configureStore({
+    reducer: {
+        employees: employeeReducer
+    }
+});
+
+export default store;
