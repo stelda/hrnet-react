6f2933c353906d9c8d41a66c84b2c1b9162cb0f0
stelda <cristel.darre@outlook.com>
2024-07-20 14:35:16 +0200

    Add routing and header component, update related CSS

---
+++ src/AppRouter.js
@@ -0,0 +1,18 @@
+﻿import React from 'react';
+import {Routes, Route, Navigate} from 'react-router-dom';
+import Error404Page from './pages/Error404Page';
+import CreateEmployeePage from "./pages/CreateEmployeePage";
+import ViewEmployeePage from "./pages/ViewEmployeePage";
+
+function AppRouter() {
+    return (
+        <Routes>
+            <Route path="/" element={<Navigate to="/new"/>}/>
+            <Route path="/new" element={<CreateEmployeePage/>}/>
+            <Route path="/list" element={<ViewEmployeePage/>}/>
+            <Route path="*" element={<Error404Page/>}/>
+        </Routes>
+    );
+}
+
+export default AppRouter;
---
+++ src/components/Header.js
@@ -0,0 +1,34 @@
+﻿import React from 'react';
+import { Link, useLocation } from 'react-router-dom';
+
+function Header() {
+    const location = useLocation();
+    return (
+        <header className="header">
+            <Link to="/">
+                <img src="/logo.png" alt="Logo Wealth Health"/>
+            </Link>
+
+            <Link to="/" className="home-link">
+                <h1>HRnet</h1>
+            </Link>
+
+            {location.pathname === '/list' ? 
+                (
+                    <Link to="/new">
+                        <button className="btn">Create Employee</button>
+                    </Link>
+                ) 
+                : 
+                (
+                    <Link to="/list">
+                        <button className="btn">List of Employees</button>
+                    </Link>
+                )
+            }
+            
+        </header>
+    );
+}
+
+export default Header;
