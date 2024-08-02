ba7b71d674c085fab7cbd44310275efc4b4db96b
stelda <stelda@outlook.fr>
2024-08-02 12:34:50 +0200

    Customize BirthDatePicker

--- src/components/DatePicker.js
+++ src/components/DatePicker.js
@@ -1,19 +1,53 @@
-﻿import React from 'react';
+﻿import React, {useState} from 'react';
 import DatePicker from 'react-datepicker';
 import 'react-datepicker/dist/react-datepicker.css';
 import '../css/custom-datepicker.css'
 
 export function BirthDatePicker({ selected, onChange }) {
+    const [selectedDate, setSelectedDate] = useState(selected);
+
     return (
         <DatePicker
-            selected={selected}
-            onChange={onChange}
+            selected={selectedDate}
+            onChange={date => {
+                setSelectedDate(date);
+                onChange(date);
+            }}
             dateFormat="MM/dd/yyyy"
-            showYearDropdown
-            yearDropdownItemNumber={124}
-            scrollableYearDropdown
-            minDate={new Date(1900, 0, 1)}
-            maxDate={new Date(2012, 11, 31)}
+            renderCustomHeader={
+                ({
+                    date,
+                    changeYear,
+                    changeMonth,
+                }) => (
+                    <div className="birthdatepicker-header">
+                        <select
+                            value={date.getFullYear()}
+                            onChange={({ target: { value } }) => changeYear(value)}
+                        >
+                            {Array.from({ length: 101 }, (_, i) => new Date().getFullYear() - i).map(year => (
+                                <option key={year} value={year}>
+                                    {year}
+                                </option>
+                            ))}
+                        </select>
+
+                        <select
+                            value={date.getMonth()}
+                            onChange={({ target: { value } }) => changeMonth(value)}
+                        >
+                            {[
+                                "January", "February", "March", "April", "May", "June",
+                                "July", "August", "September", "October", "November", "December"
+                            ].map((month, index) => (
+                                <option key={month} value={index}>
+                                    {month}
+                                </option>
+                            ))}
+                        </select>
+                    </div>
+                )
+        }
         />
     );
 }
