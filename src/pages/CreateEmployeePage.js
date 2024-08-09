483e1232eabf68de197a494d0b6ecc3e4148df85
stelda <stelda@outlook.fr>
2024-08-09 19:58:20 +0200

    Switch to react-modal-stelda package for modal implementation

--- src/pages/CreateEmployeePage.js
+++ src/pages/CreateEmployeePage.js
@@ -2,12 +2,13 @@
 import { useDispatch } from 'react-redux';
 import { addEmployee } from '../redux/employeeSlice';
 import Form from '../components/Form';
-import Modal from '../components/Modal';
+import Modal from 'react-modal-stelda';
 import '../css/main.css';
 
 function CreateEmployeePage() {
     const dispatch = useDispatch();
-    const [modalIsOpen, setModalIsOpen] = useState(false);
+
+    const [isModalOpen, setIsModalOpen] = useState(false);
 
     const saveEmployee = (employee) => {
 
@@ -23,18 +24,23 @@ function CreateEmployeePage() {
         storedEmployees.push(employee);
         localStorage.setItem('employees', JSON.stringify(storedEmployees));
         dispatch(addEmployee(employee));
-        setModalIsOpen(true);
+        setIsModalOpen(true);
     };
+
+    const closeModal = () => setIsModalOpen(false)
 
     return (
         <div>
             <div className="container">
                 <h2>Create Employee</h2>
                 <Form onSave={saveEmployee}/>
-                <Modal 
-                    isOpen={modalIsOpen}
-                    onClose={() => setModalIsOpen(false)}
-                    message="Employee created !"
+                <Modal
+                    isOpen={isModalOpen}
+                    onClose={closeModal}
+                    message="Employee Created."
+                    overlayClassName="custom-overlay"
+                    contentClassName="custom-content"
+                    closeButtonClassName="custom-close-button"
                 >
                 </Modal>
             </div>
